449c614b51d3bb80dff08357f23d059d5896a995
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-10-22 21:01:08 +0300

    debounce pure js task

---
+++ debounce.js
@@ -0,0 +1,44 @@
+// Debounce on pure js
+// Should console only last 'fetching(3)'
+
+// CONDITION
+
+// function fetchUrl(url) {
+//     return console.log(`fetching ${url}`)
+// }
+
+// function debounce(callback, ms) {
+
+// }
+
+// const fetching = debounce(fetchUrl, 300)
+
+// fetching(1)
+// fetching(2)
+// fetching(3)
+
+// SOLUTION
+
+// function fetchUrl(url) {
+//   return console.log(`fetching ${url}`);
+// }
+
+// function debounce(callback, ms) {
+//   let timer;
+
+//   return function (args) {
+//     if (timer) {
+//       clearTimeout(timer);
+//     }
+
+//     timer = setTimeout(() => {
+//       callback(args);
+//     }, ms);
+//   };
+// }
+
+// const fetching = debounce(fetchUrl, 300);
+
+// fetching(1);
+// fetching(2);
+// fetching(3);
