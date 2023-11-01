08bb9926f1f734f8e02a49c6d234675a75c2c40b
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-11-01 18:26:49 +0300

    add tasks about ts and fix binary

--- binary-tree.js
+++ binary-tree.js
@@ -4,39 +4,39 @@
 
 // const tree = {
 //   value: 1,
-//   chidlren: [
+//   children: [
 //     {
 //       value: 2,
-//       chidlren: [{ value: 3 }],
+//       children: [{ value: 3 }],
 //     },
 //     {
 //       value: 4,
-//       chidlren: [{ value: 5 }, { value: 6 }],
+//       children: [{ value: 5 }, { value: 6 }],
 //     },
 //   ],
 // };
 
 // function getTreeValues(tree) {}
 
-// console.log(getTreeValues(tree)); // => [1,2,3,4,5,6]
-
 // SOLUTION
 
 // function getTreeValues(tree) {
-//     const result = [];
-//     const stack = [tree];
+//   const stack = [tree];
+//   const result = [];
 
-//     while (stack.length > 0) {
-//       const node = stack.pop();
+//   while (stack.length > 0) {
+//     const node = stack.pop();
 
-//       if (node?.value !== undefined) {
-//         result.push(node.value);
-//       }
-
-//       if (node.chidlren?.length) {
-//         stack.push(...node.chidlren);
-//       }
+//     if (node?.value) {
+//       result.push(node.value);
 //     }
 
-//     return result;
+//     if (node.children?.length) {
+//       stack.push(...node.children);
+//     }
 //   }
+
+//   return result;
+// }
+
+// console.log(getTreeValues(tree)); // => [1,2,3,4,5,6]
