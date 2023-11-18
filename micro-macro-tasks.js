6ef939e875c5fc4a91fb47a06d9e862846921f58
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-11-18 21:32:50 +0300

    add one more micro-macro-tasks

--- micro-macro-tasks.js
+++ micro-macro-tasks.js
@@ -1,3 +1,4 @@
+// TASK 1
 // CONDITION
 
 // In what order are the numbers displayed in the console?
@@ -10,4 +11,30 @@ setTimeout(() => {
   console.log(5);
 }, 0);
 Promise.resolve().finally(() => console.log(6));
+console.log(7);
+
+// TASK 2
+// CONDITION
+
+// In what order are the numbers displayed in the console?
+
+console.log(1);
+
+setTimeout(() => {
+  console.log(2);
+});
+
+const promise1 = new Promise((resolve) => {
+  console.log(3);
+  resolve(4);
+});
+
+const promise2 = new Promise((resolve) => {
+  console.log(5);
+  resolve(6);
+});
+
+promise1.then(console.log);
+promise2.then(console.log);
+
 console.log(7);
