a1113dbd655e01f75277a2b12dd9693abda57375
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-11-19 22:41:51 +0300

    add task about filtering array items

--- arraysObjectsSort.js
+++ arraysObjectsSort.js
@@ -1,5 +1,6 @@
+// TASK 1
 // CONDITION
-// Write function thatt return ['banana', 'grapefruit', 'orange'] because banana was 3 times in array, grapefruit 2 and ...
+// Write function that return ['banana', 'grapefruit', 'orange'] because banana was 3 times in array, grapefruit - 2 and ...
 
 // let words = ["banana", "grapefruit", "banana", "grapefruit", "banana", "orange", "banana"];
 
@@ -16,3 +17,24 @@
 //   .map((item) => item[0]);
 
 // console.log(sortArr);
+
+// TASK 2
+// CONDITION
+// Write func that returns only array from nums that were repeat in inittial array
+// [1,2,3,4,2,3,2] => [2,3]
+
+// SOLUTION
+// let nums = [1, 2, 3, 4, 2, 3, 2];
+
+// const checkRepeatNums = (arr) =>
+//   Object.entries(
+//     arr.reduce((acc, item) => {
+//       acc[item] = (acc[item] || 0) + 1;
+
+//       return acc;
+//     }, {})
+//   )
+//     .filter((item) => item[1] > 1)
+//     .map((item) => +item[0]);
+
+// console.log(checkRepeatNums(nums));
