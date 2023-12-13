e9025d95562d5553668dc3b21645463ebac1eba5
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-12-13 22:45:55 +0300

    task with TS

--- aboutTypeScript.ts
+++ aboutTypeScript.ts
@@ -9,3 +9,20 @@ let s2: string = vUnknown; // Invalid; we can't assign vUnknown to any other typ
 
 vAny.method(); // Ok; anything goes with any
 vUnknown.method(); // Not ok; we don't know anything about this variable
+
+// 2) Try to type func getProperty to get Error on the second call this func ===>>> getProperty(X 'm') <=== cauze we don't have key 'm' on obj X
+
+// const x = { a: 1, b: 2, c: 3, d: 4 };
+
+// function getProperty(obj, key) {
+//     return obj[key]
+// }
+
+// getProperty(x, 'a')
+// getProperty(x, 'm')
+
+// Answear
+
+// function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
+//     return obj[key]
+// }
