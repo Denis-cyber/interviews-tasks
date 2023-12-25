a8f5a9d782bf44e6c28ef808ef824b27afc3c461
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-12-25 18:29:01 +0300

    add task about let/var/const

---
+++ let_var_const.js
@@ -0,0 +1,22 @@
+// TASK 1
+// CONDITION
+// What will be in the console?
+
+let z = 1;
+
+if (true) {
+  var f = 10;
+  let a = 10;
+  b = 4;
+  let z = 5;
+}
+
+const d = {};
+d["a"] = 10;
+d = { ...d };
+
+console.log(a);
+console.log(b);
+console.log(z);
+console.log(d);
+console.log(f);
