313086797f7e4206aa17dda65826ad71a3a95c81
Denis Bortnikov <denis.bortnikov17@gmail.com>
2023-12-25 18:41:46 +0300

    add task about prototype inheritance and references

---
+++ references.js
@@ -0,0 +1,13 @@
+// TASK 1
+// CONDITION
+// What will be in the console?
+
+let z = { d: 10 };
+let b = z;
+b.d = 11;
+
+if (b.d == z.d) console.log("Bad...");
+
+let a = 1;
+a.toString();
+if (typeof a == "number") console.log("Why ?");
