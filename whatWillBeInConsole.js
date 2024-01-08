be4d5f4992f14dfb7911f9cb5a72a1c6ee8f4d6b
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-01-08 23:10:01 +0300

    add task about console output

---
+++ whatWillBeInConsole.js
@@ -0,0 +1,19 @@
+// 1 CONDITION
+
+// console.log(typeof f1);
+// console.log(typeof f2);
+// console.log(typeof f3);
+
+// function f1() {}
+// var f2 = function () {};
+// let f3 = function () {};
+
+// 2 CONDITION
+
+// var o = {};
+// var a = [];
+
+// console.log(o == a);
+// console.log(o === a);
+// console.log(typeof o == typeof a);
+// console.log(typeof o === typeof a);
