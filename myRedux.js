ed47a7a23cbc40e48e4e89b7e30d05d168350a85
Denis Bortnikov <denis.bortnikov17@gmail.com>
2024-02-18 19:20:49 +0300

    add task about pure redux

---
+++ myRedux.js
@@ -0,0 +1,40 @@
+// CONDITION
+// Write redux on pure js?
+
+// const createStore = (paramRootReducer, paramInitialState) => {
+//   let state = paramRootReducer(paramInitialState, { type: "__INIT__" });
+//   const subscribers = [];
+
+//   return {
+//     dispatch(action) {
+//       state = paramRootReducer(state, action);
+//       subscribers.forEach((sub) => sub());
+//     },
+//     subscribe(callback) {
+//       subscribers.push(callback);
+//     },
+//     getState() {
+//       return state;
+//     },
+//   };
+// };
+
+// const rootReducer = (paramState, paramAction) => {
+//   switch (paramAction.type) {
+//     case "INCREMENT":
+//       return ++paramState;
+//     case "DECREMENT":
+//       return --paramState;
+//     default:
+//       return paramState;
+//   }
+// };
+
+// const store = createStore(rootReducer, 0);
+// store.subscribe(() => console.log("store: ", store.getState()));
+// store.dispatch({ type: "INIT_COUNTER" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "INCREMENT" });
+// store.dispatch({ type: "INCREMENT" });
