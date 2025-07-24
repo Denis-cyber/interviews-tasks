8fff751834708e29fd3f6ab724a8bee7ed9acccd
Denis Bortnikov <denis.bortnikov17@gmail.com>
2025-07-24 23:31:52 +0300

    start learn investigate digit-permutation.js

--- digit-permutation.js
+++ digit-permutation.js
@@ -1,8 +1,18 @@
 // DESCRIPTION
 
-// [1230, 199, 2301, 123, 110001, 3021, 101010, 991, 9];
-// [[199, 991], [110001, 101010], [1230, 2301, 123, 3021], [9]];
+const arrayExample = [1230, 199, 2301, 123, 110001, 3021, 101010, 991, 9];
+// [[199, 991], [110001, 101010], [1230, 2301, 1230, 3021], [9]];
 
 function digitPermutation(arr) {
-    
+    const map = new Map();
+
+    for (const item of arr) {
+        const sortedItem = String(item).split('').sort().join('');
+
+        if (!map.has(sortedItem)) {
+            map.set(sortedItem, value)
+        }
+    }
 }
+
+digitPermutation(arrayExample);
