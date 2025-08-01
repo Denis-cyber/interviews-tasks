877d3239189a9545a9b078ced7333842f9c72d46
Denis Bortnikov <denis.bortnikov17@gmail.com>
2025-08-01 20:28:06 +0300

    debounce-js edit [2]

--- debounce-js.js
+++ debounce-js.js
@@ -3,7 +3,7 @@
 // Write your debounce on pure js
 // Should console only last 'fetching(3)'
 
-// function fetchUrl(url) {
+// const fetchUrl = (url) => {
 //     return console.log(`fetching ${url}`)
 // }
 
@@ -17,10 +17,59 @@
 // fetching(2)
 // fetching(3)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SOLUTION
 
-// function fetchUrl(url) {
-//   return console.log(`fetching ${url}`);
+// const fetchUrl = (url) => {
+//     return console.log(`fetching ${url}`)
+// }
+
+// function debounce(callback, ms) {
+//     let timer;
+  
+//     return (args) => {
+//         timer && clearTimeout(timer)
+
+//         timer = setTimeout(() => {
+//             callback(args)
+//         }, ms)
+//     }
+// }
+
+// const fetching = debounce(fetchUrl, 300);
+
+// fetching(1);
+// fetching(2);
+// fetching(3);
+
+
+
+
+
+
+// UPDATE 
+// If you have written debounce correct - conditions changed)
+
+
+// const fetchUrl = (url) => {
+//     return console.log(`fetching ${url}`, this.firstName)
+// }
+
+// const user = {
+//     firstName: 'Bob',
 // }
 
 // function debounce(callback, ms) {
@@ -36,6 +85,34 @@
 // }
 
 // const fetching = debounce(fetchUrl, 300);
+
+// fetching(1);
+// fetching(2);
+// fetching(3);
+
+// In console should print not only number, but also and name 'Bob' 
+// Debounce function don't touch
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOLUTION
+
+// function fetchUrl(url)  {
+//     return console.log(`fetching ${url}`, this.firstName)
+// }
+
+// const fetching = debounce(fetchUrl.bind(user), 300);
 
 // fetching(1);
 // fetching(2);
