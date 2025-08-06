5f544423aa938f28dfe1839a06a4d6bbc7667f6e
Denis Bortnikov <denis.bortnikov17@gmail.com>
2025-08-06 10:55:03 +0300

    practice map-filter

--- map-filter.ts
+++ map-filter.ts
@@ -38,14 +38,21 @@
 
 // SOLUTION
 
-// function myMap<T>(arr: T[], callback: (item: T, index: number, arr: T[]) => T) {
-//   return arr.reduce((acc: T[], value: T, index: number, arr: T[]) => {
+// function myMap<T,U>(
+//     arr: T[], 
+//     callback: (item: T, index: number, arr: T[]) => U
+// ): U[] {
+//   return arr.reduce((acc: U[], value: T, index: number, arr: T[]) => {
 //     acc.push(callback(value, index, arr));
 //     return acc;
 //   }, []);
 // }
 
 // console.log(myMap([1, 2, 3, 4, 5], (el) => el * 2));
+
+
+
+
 
 // CONDITION
 // 2) write filter function (for example myFilter) through the method reduce with types
@@ -56,13 +63,42 @@
 
 // console.log(myFilter([1, 2, 3, 4, 5], (el) => el > 2));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SOLUTION
 
-// function myFilter<T>(arr: T[], callback: (item: T, index: number, arr: T[]) => boolean) {
-//   return arr.reduce((acc: T[], value: T, indexReduce: number, arrReduce: T[]) => {
-//     if (callback(value, indexReduce, arrReduce)) {
-//       acc.push(value);
+// function myFilter<T>(
+//     arr: T[],
+//     callback: (item: T, index: number, arr: T[]) => boolean,
+// ): T[] {
+//   return arr.reduce((acc: T[], value: T, index: number, arr: T[]) => {
+//     if (callback(value, index, arr)) {
+//         acc.push(value)
 //     }
+
 //     return acc;
 //   }, []);
 // }
