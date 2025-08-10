165c8b7b8a75ba0f9a3c2f24be19356d9d9126af
Denis Bortnikov <denis.bortnikov17@gmail.com>
2025-08-10 16:10:18 +0300

    practice with let-var-const

--- let-var-const.js
+++ let-var-const.js
@@ -20,3 +20,53 @@ console.log(b);
 console.log(z);
 console.log(d);
 console.log(f);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOLUTION
+
+
+console.log(a); // a is not defined, because block scope
+console.log(b); // 4, because default (without var / let const) will equall global scope
+console.log(z); // 1, because 5 - only in block scope
+console.log(d); // error, because d = const and we can't write d = { ...d };
+console.log(f); // 10, hoisting and var
