078a74040af879534954a1a124e829fb8e0a8e56
Denis Bortnikov <denis.bortnikov17@gmail.com>
2025-08-13 15:33:50 +0300

    practice with promise all

--- promise-all.js
+++ promise-all.js
@@ -1,39 +1,65 @@
 // CONDITION
 // Write your PromiseAll
 
-// const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
 
-// function fetch1() {
-//   return sleep(500).then(() => 22);
-// }
+function fetch1() {
+  return sleep(500).then(() => 22);
+}
 
-// function fetch2() {
-//   return sleep(200).then(() => 10);
-// }
+function fetch2() {
+  return sleep(200).then(() => 10);
+}
 
-// function runInParallel(fetch1, fetch2) {
-//   return Promise.all([fetch1(), fetch2()]);
-// }
+Promise.all([fetch1(), fetch2()]).then(console.log); // instead Promise.all use PromiseAll
 
-// runInParallel(fetch1, fetch2).then(console.log);
 
-// SOLUTION
 
-// function myPromiseAll(promises) {
-//   const res = [];
-//   const promisesLength = promises.length;
 
-//   return new Promise((resolve, reject) => {
-//     for (let i = 0; i < promisesLength; i++) {
-//       promises[i]
-//         .then((response) => {
-//           res.push(response);
 
-//           if (promisesLength === res.length) {
-//             resolve(res);
-//           }
-//         })
-//         .catch((err) => reject(err));
-//     }
-//   });
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//SOLUTION
+
+function myPromiseAll(promises) {
+  const res = [];
+  const promisesLength = promises.length;
+
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < promisesLength; i++) {
+      promises[i]
+        .then((response) => {
+          res.push(response);
+
+          if (promisesLength === res.length) {
+            resolve(res);
+          }
+        })
+        .catch((err) => reject(err));
+    }
+  });
+}
