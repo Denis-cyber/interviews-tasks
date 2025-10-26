c3a6e2169d18318d8f8794109d2fc14128ef6d48
denis.bortnikov <denis.bortnikov@agcsoft.com>
2025-10-26 19:19:01 +0300

    add task about react-refactor

--- react-refactor.js
+++ react-refactor.js
@@ -1,1 +1,35 @@
-// add task
+// @ts-nocheck
+/* eslint-disable */
+
+// CONDITION
+
+// import React;
+// import sendMetric from 'metrics';
+// import sendData from 'data';
+// import bigComputations from 'utils';
+
+// const pleaseReviewMe = props => {
+//   const [data, setData] = React.useState(bigComputations(props.argument));
+
+//   const [items] = useState([{id: 1}, {id: 2}, {id: 3}]);
+
+//   React.useEffect(() => {
+//     document.addEventListener('click', () => {
+//       sendMetric('click')
+//     })
+//   })
+
+//   const click = React.useCallback(id => {
+//     sendData(data, id);
+//   })
+
+//   return (
+//     <React.Fragment>
+//       {items.map(item => (
+//         <div onClick={() => click(item.id)}>{item.id}</div>
+//       ))}
+//     </React.Fragment>
+//   )
+// }
+
+// export pleaseReviewMe;
