0d5f8d92537fa2d76c2f6faf335aed30a5a5d208
Denis Bortnikov <denis.bortnikov17@gmail.com>
2026-01-20 16:40:29 +0300

    add 2 solutions for algorithms

--- algorithms.js
+++ algorithms.js
@@ -4,7 +4,7 @@
 // write code, that would be reverse array
 
 function reverse (arr) {
-    // code
+    // code;
 }
 
 console.log(reverse(['h', 'e', 'l', 'l', 'o'])); // ['o', 'l', 'l', 'e', 'h']
@@ -37,5 +37,63 @@ console.log(reverse(['t', 'o', 'p', 's', 'k','y'])); // ['y', 'k', 's', 'p', 'o'
 
 
 
-// solution 1
+// solution 1 (simply)
 
+// function reverse (arr) {
+//     return arr.reverse()
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// solution 2 (more difficult - without array.prototype.reverse)
+
+// function reverse (arr) {
+//     const result = [];
+//     const lastIndex = arr.length - 1;
+
+//     let i = 0; i > 0; i++
+
+//     for (let i = lastIndex; i >= 0; i-- ) {
+//         result.push(arr[i]);
+//     }
+
+//     return result;
+// }
