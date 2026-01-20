08499d0cb1cb76d40e65f597277a417025cb290e
Denis Bortnikov <denis.bortnikov17@gmail.com>
2026-01-20 16:27:59 +0300

    add task about reverse (algorithms)

---
+++ algorithms.js
@@ -0,0 +1,41 @@
+// TASK N 1
+
+// condition 
+// write code, that would be reverse array
+
+function reverse (arr) {
+    // code
+}
+
+console.log(reverse(['h', 'e', 'l', 'l', 'o'])); // ['o', 'l', 'l', 'e', 'h']
+console.log(reverse(['t', 'o', 'p', 's', 'k','y'])); // ['y', 'k', 's', 'p', 'o','t']
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// solution 1
+
