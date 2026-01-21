ad986487b57a5af8ba6c68121f48829a48848444
Denis Bortnikov <denis.bortnikov17@gmail.com>
2026-01-21 22:47:01 +0300

    add task abount finding max string

---
+++ a.findMaxString.js
@@ -0,0 +1,104 @@
+// condition
+
+const findMaxString = (s) => {
+    // code
+}
+
+console.log(findMaxString('aaa bbbbb cc')); // 5
+console.log(findMaxString('a bbb ccc dd')); // 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// solution easy (beacause there are 2 arrays iterations)
+
+// const findMaxString = (s) => s
+//     .split(' ').reduce((acc, item) => {
+//         if (item.length > acc) {
+//             acc = item.length
+//         }
+
+//         return acc;
+
+//     }, 0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// solution hard (with 1 iteration)
