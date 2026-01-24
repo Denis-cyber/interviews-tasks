a24367632209ac3b67a275355c1d90f321c353f1
Denis Bortnikov <denis.bortnikov17@gmail.com>
2026-01-24 23:04:43 +0300

    add mirror (algorithms)

---
+++ algorithms/mirror.js
@@ -0,0 +1,11 @@
+// Condition
+
+// Arrays can be of different lengths.
+// Numbers in the arrays can be both positive and negative.
+
+function mirror(arr1, arr2) {
+    // code
+}
+
+console.log(mirror([1, 2, 3, 4], [4, 3, 2, 1]));// true
+console.log(mirror([1, 2, 3], [3, 2, 1, 0]));// false
