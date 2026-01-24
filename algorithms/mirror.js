8e3839da2886466f85a5268af6c6d20d8d39d161
Denis Bortnikov <denis.bortnikov17@gmail.com>
2026-01-24 23:31:14 +0300

    mirror solution

--- algorithms/mirror.js
+++ algorithms/mirror.js
@@ -2,10 +2,83 @@
 
 // Arrays can be of different lengths.
 // Numbers in the arrays can be both positive and negative.
+// Write solution with only one iteration
 
 function mirror(arr1, arr2) {
-    // code
+    // code;    
 }
 
 console.log(mirror([1, 2, 3, 4], [4, 3, 2, 1]));// true
-console.log(mirror([1, 2, 3], [3, 2, 1, 0]));// false
+console.log(mirror([1, 2, 3], [3, 2, 1, 0]));// false 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// solution 
+
+// function mirror(arr1, arr2) {
+
+//     if (arr1.length !== arr2.length) return false;
+
+//     const iterationNumber = arr1.length;
+//     let counterFromLastIndex = iterationNumber - 1;
+
+//     for (let i = 0; i < iterationNumber; i++) {
+//         if (arr1[i] !== arr2[counterFromLastIndex]) {
+//             return false
+//         }
+
+//         counterFromLastIndex--;
+
+//         if (i === iterationNumber - 1) {
+//             return true
+//         }
+//     }
+    
+// }
